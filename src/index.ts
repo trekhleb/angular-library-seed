1aa33018bf12545287b9f8b0d9a96c7b04de1d86
Tarun Kumar Sukhu <tksukhu@gmail.com>
2017-10-25 23:12:25 +0530

    removed redundant plans module

--- src/index.ts
+++ src/index.ts
@@ -28,7 +28,6 @@ export {
 export {
     TickTockModule,
     LayoutModule,
-    PlansModule,
     OrderDetailsModule,
     CartModule,
     RegisterFormModule
--- src/modules/index.ts
+++ src/modules/index.ts
@@ -1,6 +1,5 @@
 export * from './layout.module';
 export * from './tick-tock.module';
-export * from './plans.module';
 export * from './order-details.module';
 export * from './cart.module';
 export * from './register-form.module';
